287afebbd87c9e38ec543cecf336eab8248e4cb5
David Adams <david.adams.16.12@gmail.com>
2024-03-24 20:10:45 +1100

    Fix suffix pgn showing as correct when there is no suffix

--- src/components/PGNGroup.tsx
+++ src/components/PGNGroup.tsx
@@ -113,7 +113,12 @@ function PGNGroup<P extends ParsingKey & ('pgn' | 'suffix')>({
     [part, verb],
   )
   const isCorrectOption = useCallback(
-    (option: PGN) => Object.values(checkPGN(option, correctAnswer, parsing)).every(Boolean),
+    (option: PGN) => {
+      if (part === 'suffix' && !hasSetPGN(verb.suffix)) {
+        return false
+      }
+      return Object.values(checkPGN(option, correctAnswer, parsing)).every(Boolean)
+    },
     [correctAnswer, parsing],
   )
   const isApplicableOption = useCallback(

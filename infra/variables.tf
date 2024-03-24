d2aab338088b56f6b8bf00ad04db7bf933966c39
David Adams <david.adams.16.12@gmail.com>
2024-03-25 09:31:24 +1100

    Remove old TF variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,22 +7,3 @@ variable "code_bucket" {
   type    = string
   default = "crosscode-lambdas"
 }
-
-variable "ccb_username" {
-  type = string
-}
-
-variable "ccb_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "bgf_password_salt" {
-  type      = string
-  sensitive = true
-}
-
-variable "bgf_password_hash" {
-  type      = string
-  sensitive = true
-}

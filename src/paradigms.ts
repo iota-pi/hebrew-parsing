e76c8892393700a2be4a3949cdfdcaaf687ef3e7
David Adams <david.adams.16.12@gmail.com>
2024-04-02 17:48:48 +1100

    Handle wayyiqtol ambiguous forms

--- src/paradigms.ts
+++ src/paradigms.ts
@@ -11,4 +11,12 @@ export const equivalents: ParsingCondition[][] = [
     { tense: 'Yiqtol', pgn: { person: 3, gender: 'f', number: 'p' } },
     { tense: 'Yiqtol', pgn: { person: 2, gender: 'f', number: 'p' } },
   ],
+  [
+    { tense: 'Wayyiqtol', pgn: { person: 2, gender: 'm', number: 's' } },
+    { tense: 'Wayyiqtol', pgn: { person: 3, gender: 'f', number: 's' } },
+  ],
+  [
+    { tense: 'Wayyiqtol', pgn: { person: 3, gender: 'f', number: 'p' } },
+    { tense: 'Wayyiqtol', pgn: { person: 2, gender: 'f', number: 'p' } },
+  ],
 ]

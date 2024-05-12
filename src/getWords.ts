9a4605d8215a5f7b867371eb72880d3b250fdfb6
David Adams <david.adams.16.12@gmail.com>
2024-05-12 18:11:12 +1000

    Fix linting

--- src/getWords.ts
+++ src/getWords.ts
@@ -4,8 +4,8 @@ import { getLinkedOccurrences } from './loadData'
 
 function shuffleArray<T>(array: T[]): T[] {
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array
 }
